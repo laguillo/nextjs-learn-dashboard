abe69216c40d13e2364b34a007d2b573dc30263c
Eduardo Lago <laguillo@gmail.com>
2024-11-11 17:37:33 -0500

    Pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,38 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import { CreateInvoice } from "@/components/invoices/buttons";
+import Pagination from "@/components/invoices/pagination";
+import Table from "@/components/invoices/table";
+import Search from "@/components/search";
+import { InvoicesTableSkeleton } from "@/components/skeletons";
+import { fetchInvoicesPages } from "@/lib/data";
+import { Suspense } from "react";
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl">Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
