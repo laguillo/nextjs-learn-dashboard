8ae3a9f6f380aa5ddd7fd35584160381b117f8ac
Eduardo Lago <laguillo@gmail.com>
2024-11-11 12:24:11 -0500

    Commit Inicial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
+import "./global.css";
+import { Inter } from "next/font/google";
+
+export const fontSans = Inter({ subsets: ["latin"] });
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body className={`${fontSans.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
