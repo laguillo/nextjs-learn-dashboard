f2703d71c0501085cf1a16749456181c5f296bb7
Eduardo Lago <laguillo@gmail.com>
2024-11-11 21:37:12 -0500

    WIP

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,33 @@
+import Breadcrumbs from "@/components/invoices/breadcrumbs";
+import EditInvoiceForm from "@/components/invoices/edit-form";
+import { fetchCustomers, fetchInvoiceById } from "@/lib/data";
+import { notFound } from "next/navigation";
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+  
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  );
+}
